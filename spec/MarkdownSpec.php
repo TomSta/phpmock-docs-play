e1f7ec6e0af84b0057c5b89786ea9ec6b2c403b4
Tommy <tom.staszyszyn@gmail.com>
2016-02-20 17:21:52 +0000

    introduce stub

--- spec/MarkdownSpec.php
+++ spec/MarkdownSpec.php
@@ -5,6 +5,7 @@ namespace spec;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 
+use Markdown\Reader;
 class MarkdownSpec extends ObjectBehavior
 {
     function it_is_initializable()
@@ -16,5 +17,15 @@ class MarkdownSpec extends ObjectBehavior
     {
       $this->toHtml("Hello world from PHPSpec")
               ->shouldReturn("<div>Hello world from PHPSpec</div>");
+    }
+
+    function it_converts_text_from_external_source(Reader $reader)
+    {
+      $reader->getMarkDown()
+              ->willReturn("Hello world from PHPSpec");
+
+      $this->toHtmlFromReader($reader)
+              ->shouldReturn("<div>Hello world from PHPSpec</div>");
+
     }
 }
--- src/Markdown.php
+++ src/Markdown.php
@@ -1,9 +1,15 @@
 <?php
+use Markdown\Reader;
 
 class Markdown
 {
-  public function toHtml(string $string) :string
+  public function toHtml( string $string ) :string
   {
     return "<div>".$string."</div>";
   }
+
+    public function toHtmlFromReader( Reader $reader ) :string
+    {
+        return $this->toHtml( $reader->getMarkDown() );
+    }
 }
